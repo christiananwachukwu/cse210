ec60b1f5593ebcc233d2fda0249bf006b5c0e9b0
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-05-11 05:40:49 +0100

    add modified exercises 1-5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+        Random randomGenerator = new Random();
+        Console.WriteLine("Welcome to the number guessing game!");
+
+        while (playAgain)
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int attempts = 0;
+
+            Console.WriteLine("\nI'm thinking of a number between 1 and 100.");
+
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    attempts++;
+                    if (guess < 1 || guess > 100)
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 100.");
+                    }
+                    else if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it in {attempts} attempts!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+            playAgain = (response == "yes");
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,52 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number;
+
+        while (!int.TryParse(input, out number))
+        {
+            Console.Write("Invalid input. Please enter a valid number: ");
+            input = Console.ReadLine();
+        }
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }

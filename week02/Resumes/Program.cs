c5151d536ba41ae88156eadc64d007ef4830a0a6
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-05-12 23:20:01 +0100

    created job and resume classes and main

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new Resume("Christiana Nwachukwu");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Health Education Teacher";
+        job1._company = "F.C.G.C.S.School";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "BYU Idaho";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+    
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

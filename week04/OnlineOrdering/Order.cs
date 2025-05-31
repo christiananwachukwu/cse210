24314531e577ec11e5480c2c780c037de9e92329
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-05-31 22:22:50 +0100

    week04 onlineordering

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (Product product in products)
+        {
+            total += product.GetTotalCost();
+        }
+        total += customer.LivesInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder label = new StringBuilder("Packing Label:\n");
+        foreach (Product product in products)
+        {
+            label.AppendLine(product.GetPackingLabel());
+        }
+        return label.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{customer.GetName()}\n{customer.GetAddress().GetFullAddress()}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // First order
+        Address addr1 = new Address("5 Main Street", "Springfield", "IL", "USA");
+        Customer cust1 = new Customer("Chistiana Nwachukwu", addr1);
+        Order order1 = new Order(cust1);
+        order1.AddProduct(new Product("Laptop", "P100", 900, 1));
+        order1.AddProduct(new Product("Mouse", "P101", 25, 2));
+
+        // Second order
+        Address addr2 = new Address("45 Iwuanyanwu Close", "Ikenebu", "Enugwu", "Nigeria");
+        Customer cust2 = new Customer("Queen Esther", addr2);
+        Order order2 = new Order(cust2);
+        order2.AddProduct(new Product("Camera", "P200", 350, 2));
+        order2.AddProduct(new Product("Tripod", "P201", 100, 2));
+        order2.AddProduct(new Product("Bag", "P202", 40, 1));
+
+        DisplayOrder(order1);
+        Console.WriteLine("-----------------------");
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.GetTotalCost():0.00}");
     }
 }
+

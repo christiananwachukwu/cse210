1c052d3aa96a978ae57dffe8e9f3762fbaa03cb7
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-06-02 20:03:39 +0100

    MathAssignment and WritingAssignment added to Homework list

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("Christiana Nwachukwu", "Fractions", "7.3", "3-10");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War 11");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
